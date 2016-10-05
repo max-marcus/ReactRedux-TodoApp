7c836a932cd0f6a9985d23650836bba04648fe39
Michael Blanchard <michael.james.blanchard@gmail.com>
2016-10-05 14:41:34 -0700

    added boilerplate from redux branch into master branch

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
+import todoReducer from './todoReducer';
 
 const reducers = combineReducers({
-  
+  // if you have multiple reducers you can add them below todo
+  todo: todoReducer,
 });
-
 
 export default reducers;
