19dd554c24b3dd614510d37dc42d8cccd49dce20
Michael Blanchard <michael.james.blanchard@gmail.com>
2016-10-06 10:37:42 -0700

    updated index.js to remove deprecated App render inside AppContainer

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-
 import App from './containers/App';
 
-render(
-  <AppContainer
-    component={App}
-  />,
-  document.getElementById('app')
-);
+const Default = App.default;
+
+render(<AppContainer><App /></AppContainer>, document.getElementById('app'));
 
 if (module.hot) {
   module.hot.accept('./containers/App', () => {
-    render(
-      <AppContainer
-        component={require('./containers/App').default}
-      />,
-      document.getElementById('app')
-    );
+    render(<AppContainer><Default /></AppContainer>, document.getElementById('app'));
   });
 }
