d189c5f7517aec1be8a0b35adcfe891e38c28bfb
Michael Laythe <mrlaythe24@aol.com>
2016-12-09 10:20:19 -0800

    fix hot reloading

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import App from './containers/App';
 
-const Default = App.default;
-
 render(<AppContainer><App /></AppContainer>, document.getElementById('app'));
 
 if (module.hot) {
   module.hot.accept('./containers/App', () => {
-    render(<AppContainer><Default /></AppContainer>, document.getElementById('app'));
+    const NextApp = require('./containers/App').default;
+    render(<AppContainer><NextApp /></AppContainer>, document.getElementById('app'))
   });
 }
