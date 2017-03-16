ef6f98bc662c19e40aa911540967e02f3350dec3
listentomm <maxmarcususc@gmail.com>
2017-03-15 18:09:44 -0700

    started connecting dispatchers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,9 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 import TodoContainer from './TodoContainer';
+import store from './../store';
+import * as types from './../actions/actionTypes';
+import * as actions from './../actions/todoActions';
 
 class App extends Component {
   render() {
@@ -8,6 +12,22 @@ class App extends Component {
         <TodoContainer />
       </div>
     );
+  }
+}
+
+const mapStateToProps = function(store) {
+  return {
+    tasks: store.tasks,
+    taskName: store.taskName,
+  };
+};
+
+const mapDispatchToProps = function(dispatch) {
+  return {
+    changeTaskName: (target) => {
+      dispatch(actions.updateTaskActionCreator(target));
+    },
+    addTask: ()
   }
 }
 
